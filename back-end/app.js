dc4122813a8bcb3a531ef6b901aa4170413b8704
Kiran <kiranchauhan68@gmail.com>
2022-05-29 18:49:59 +0530

    some re-arrangement

--- back-end/app.js
+++ back-end/app.js
@@ -11,6 +11,15 @@ app.get("/check", (req, res) => {
   res.json({ data: "Application is up & running!" });
 });
 
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "4-0-4" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ error: "Internal server error" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Application is up & running at ${PORT}!`);
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+
+import "bootstrap/dist/js/bootstrap.bundle";
 
 import App from "./App";
 
